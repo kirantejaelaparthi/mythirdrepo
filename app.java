fee659a83e31e742a021bbaa8d8b6efd1af593d3
kiran teja <90080709+kirantejaelaparthi@users.noreply.github.com>
2025-08-12 16:22:29 +0530

    Update app.java

--- app.java
+++ app.java
@@ -15,4 +15,4 @@ public class EvenOdd {
             System.out.println(num + " is odd");
     }
 }
-making some little changes for understanding
+making the required changes that we can do for ourselves
